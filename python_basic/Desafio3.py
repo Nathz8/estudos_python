3c8e44f22c87d5f175537814faba08f93c10e75a
Nathz8 <nathan.correa3103@gmail.com>
2024-07-16 21:44:38 -0300

    Desafios

---
+++ python_basic/Desafio3.py
@@ -0,0 +1,8 @@
+numero1 = input ('Primeiro número:')
+numero2 = input ('Segundo número:')
+print ('A soma é', numero1 + numero2)
+
+
+numero1 = input ('Primeiro número:')
+numero2 = input ('Segundo número:')
+print(valor)

ce2ebe8b270895918162c837dfc2513024bca6c3
Nathan Caldas <89230478+Nathz8@users.noreply.github.com>
2024-07-17 09:46:34 -0300

    Update Desafio3.py

--- python_basic/Desafio3.py
+++ python_basic/Desafio3.py
@@ -1,8 +1,10 @@
 numero1 = input ('Primeiro número:')
 numero2 = input ('Segundo número:')
-print ('A soma é', numero1 + numero2)
+print ('A soma é:', numero1 + numero2)
 
+print()
 
-numero1 = input ('Primeiro número:')
-numero2 = input ('Segundo número:')
-print(valor)
+valor1 = int(input ('Primeiro número:'))
+valor2 = int(input ('Segundo número:'))
+resultado = valor1 + valor2
+print('A soma é:', resultado)
